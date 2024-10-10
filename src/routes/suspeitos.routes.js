146683a7a413816ed17db7028f0fcd841bade839
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2024-10-10 15:45:03 -0300

    feat: lista dos suspeitos

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -3,10 +3,27 @@ import { Router } from "express";
 const suspeitosRoutes = Router();
 let suspeitos = [
     {
-        id 
-    }
+        id: Math.floor(Math.random() * 1000000),
+        nome: "Neymar",
+        profissao: "Jogador",
+        envolvimentoApostas: "sim",
+        nivelSuspeita: "baixa",
+    },
+    {
+        id: Math.floor(Math.random() * 1000000),
+        nome: "Jon Vlogs",
+        profissao: "influenciador",
+        envolvimentoApostas: "sim",
+        nivelSuspeita: "media",
+    },
+    {
+        id: Math.floor(Math.random() * 1000000),
+        nome: "Felipe Neto",
+        profissao: "Youtuber",
+        envolvimentoApostas: "sim",
+        nivelSuspeita: "baixa",
+    },
 ];
-
 
 
 // 1. Rota para cadastrar um novo suspeito
@@ -23,7 +40,7 @@ const novoSuspeito = {
     nivelSuspeita,
 };
 
-// 1. Adiciona o novo candidato ao array de candidatos
+// 1. Adiciona o novo suspeito ao array de suspeitos
 suspeitos.push(novoSuspeito);
 
     return res.status(201).json({
@@ -41,5 +58,24 @@ suspeitosRoutes.get("/:id", (req,res) => {
     const { id } = req.params;
 });
 
-// 3. Busca um suspeito pelo id no array de candidatos
+// 3. Busca um suspeito pelo id no array de suspeitos
 const suspeito = suspeitos.find((ladrao) => ladrao.id == id);
+
+// 4. Rota para atualizar um suspeito pelo id
+suspeitosRoutes.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { nome, profissao, envolvimentoApostas, nivelSuspeita } = req.body;
+
+// 4. Busca de um suspeito pelo id no array de suspeitos
+    const suspeito = suspeitos.find((landrao) => ladrao.id == id)
+});
+
+// 5. Remove o suspeito do array de suspeitos
+candidatos = candidatos.filter((candidato) => candidato.id != id);
+
+return res.status(200).json({
+    message: "Suspeito removido com sucesso!",
+    suspeito,
+});
+
+export default suspeitosRoutes;
